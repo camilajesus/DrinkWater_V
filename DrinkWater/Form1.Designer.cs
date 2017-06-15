820df28dba4d217f051f56f4b43c77d8da72b605
camilajesus <camilaajjesus@gmail.com>
2017-06-15 18:08:06 -0300

    Add project files.

---
+++ DrinkWater/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace DrinkWater
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtHora = new System.Windows.Forms.TextBox();
+            this.txtMinuto = new System.Windows.Forms.TextBox();
+            this.btnAtivar = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblMinuto = new System.Windows.Forms.Label();
+            this.lblHora = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtHora
+            // 
+            this.txtHora.Location = new System.Drawing.Point(13, 136);
+            this.txtHora.Name = "txtHora";
+            this.txtHora.Size = new System.Drawing.Size(100, 20);
+            this.txtHora.TabIndex = 0;
+            // 
+            // txtMinuto
+            // 
+            this.txtMinuto.Location = new System.Drawing.Point(135, 136);
+            this.txtMinuto.Name = "txtMinuto";
+            this.txtMinuto.Size = new System.Drawing.Size(100, 20);
+            this.txtMinuto.TabIndex = 1;
+            this.txtMinuto.TextChanged += new System.EventHandler(this.txtMinuto_TextChanged);
+            // 
+            // btnAtivar
+            // 
+            this.btnAtivar.Location = new System.Drawing.Point(161, 217);
+            this.btnAtivar.Name = "btnAtivar";
+            this.btnAtivar.Size = new System.Drawing.Size(121, 63);
+            this.btnAtivar.TabIndex = 2;
+            this.btnAtivar.Text = "Ativar";
+            this.btnAtivar.UseVisualStyleBackColor = true;
+            this.btnAtivar.Click += new System.EventHandler(this.btnAtivar_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblMinuto
+            // 
+            this.lblMinuto.AutoSize = true;
+            this.lblMinuto.Location = new System.Drawing.Point(376, 139);
+            this.lblMinuto.Name = "lblMinuto";
+            this.lblMinuto.Size = new System.Drawing.Size(69, 13);
+            this.lblMinuto.TabIndex = 3;
+            this.lblMinuto.Text = "Cont Minutos";
+            this.lblMinuto.Click += new System.EventHandler(this.lblMinuto_Click);
+            // 
+            // lblHora
+            // 
+            this.lblHora.AutoSize = true;
+            this.lblHora.Location = new System.Drawing.Point(310, 139);
+            this.lblHora.Name = "lblHora";
+            this.lblHora.Size = new System.Drawing.Size(60, 13);
+            this.lblHora.TabIndex = 4;
+            this.lblHora.Text = "Cont Horas";
+            this.lblHora.Click += new System.EventHandler(this.lblHora_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 120);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Horas";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(167, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Minutos";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(119, 139);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(12, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = ":";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(141, 31);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(168, 31);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Drink Water";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(452, 326);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblHora);
+            this.Controls.Add(this.lblMinuto);
+            this.Controls.Add(this.btnAtivar);
+            this.Controls.Add(this.txtMinuto);
+            this.Controls.Add(this.txtHora);
+            this.Name = "Form1";
+            this.Text = "Drink Water";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtHora;
+        private System.Windows.Forms.TextBox txtMinuto;
+        private System.Windows.Forms.Button btnAtivar;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblMinuto;
+        private System.Windows.Forms.Label lblHora;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ DrinkWater/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DrinkWater
+{
+    public partial class Form1 : Form
+    {
+        int hora;
+        int minuto;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAtivar_Click(object sender, EventArgs e)
+        {
+            if (btnAtivar.Text == "Ativar")
+            {
+                btnAtivar.Text = "Desativar";
+
+                hora = Convert.ToInt16(txtHora.Text);
+                minuto = Convert.ToInt16(txtMinuto.Text);
+                lblHora.Text = Convert.ToString(hora);
+                timer1.Start();
+            }
+            else timer1.Stop();
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (minuto > 0)
+            {
+                minuto--;
+                lblMinuto.Text = Convert.ToString(minuto);
+
+
+            }
+            else
+            {
+                if (hora > 0)
+                {
+                    hora--;
+
+
+                    minuto = 60;
+                }
+                lblHora.Text = Convert.ToString(hora);
+            }
+            if (minuto == 0 && hora == 0)
+            {
+                timer1.Stop();
+                MessageBox.Show("Vá se hidratar, hora de beber água!");
+                hora = Convert.ToInt16(txtHora.Text);
+                minuto = Convert.ToInt16(txtMinuto.Text);
+                lblHora.Text = Convert.ToString(hora);
+                timer1.Start();
+            }
+
+        }
+
+        private void lblHora_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblMinuto_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtMinuto_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
